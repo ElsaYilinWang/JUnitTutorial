ca8a4c12d3e9c58a4cb531015a3f28a3ead6bd6a
elsawang <elsa.yilin.w39@gmail.com>
2024-11-08 13:21:08 +0000

    JUnit tutorial @MethodSource

--- src/test/java/BlankTest.java
+++ src/test/java/BlankTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,8 +17,39 @@ class BlankTest {
     @ValueSource(strings = {"  ", "\t", "\n"})
     @NullAndEmptySource
     void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+
         assertTrue(Blank.isBlank(input));
     }
 
+    // to pass complex objects, use a method as an argument source
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")  // name supplied should match an existing method
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input, boolean expected){
+        assertEquals(expected, Blank.isBlank(input));
+    }
+
+    private static Stream<Arguments> provideStringsForIsBlank(){
+        return Stream.of(
+            Arguments.of(null, true),
+            Arguments.of("", true),
+            Arguments.of("  ", true),
+            Arguments.of("not blank", false)
+        );
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
